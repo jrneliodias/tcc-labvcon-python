8baccb28afcf382d7a2d94015f7f77387ab8a285
Nélio Dias <jrneliodias@gmail.com>
2023-09-04 22:41:16 -0300

    erase comments

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 from serialApp import serialApp
 import time
-# portlist = [port.device for port in comports()]
-# print(portlist)
-# arduinoData = serial.Serial('COM9', 250000)
-
-# time.sleep(1)
-
-
-# cmd = input('Please Enter Your command: ')
-# cmd = cmd + '\n'
-
-# arduinoData.write(cmd.encode())
-
-# while (arduinoData.in_waiting == 0):
-#     pass
-# dataPacket = arduinoData.readline()
-# print(dataPacket)
 
 # Instanciar o objeto
 arduino = serialApp()
