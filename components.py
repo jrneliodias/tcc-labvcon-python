f75df5e59b04e2383c8726accc9dc5794b0438e6
Nélio Dias <jrneliodias@gmail.com>
2023-09-09 00:55:30 -0300

    serial speed error test

--- components.py
+++ components.py
@@ -1,6 +1,7 @@
 from connections import *
 import streamlit as st
 import pandas as pd
+import datetime
 
 
 def sidebarMenu():
@@ -38,12 +39,13 @@ def sidebarMenu():
     if st.button('Receber Dados'):
         if 'arduinoData' in st.session_state.connected:
             arduinoData = st.session_state.connected['arduinoData']
-            st.session_state.sensor = 800*[0]
+            st.session_state.sensor = dict()
             sensor = st.session_state.sensor
             for i in range(800):
                 sendToArduino(arduinoData, 'ON')
-                dataRead = readFromArduino(arduinoData, 0.1)
-                sensor[i] = float(dataRead)
+                dataRead = readFromArduino(arduinoData, 0)
+                current_timestamp = datetime.datetime.now()
+                sensor[str(current_timestamp)] = float(dataRead)
             st.write(dataRead)
         else:
             st.warning('Não há dispositivos conectados.')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from components import *
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+from datetime import datetime, timedelta
 
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
@@ -36,3 +37,30 @@ with st.sidebar:
 
 
 st.line_chart(data=st.session_state.sensor)
+
+datetimeList = list(st.session_state.sensor.keys())
+st.write(list(st.session_state.sensor.keys()))
+
+# Your date string
+date_string = "2023-09-08 23:40:16.200179"
+
+# Define the format of your date string
+date_format = "%Y-%m-%d %H:%M:%S.%f"
+
+# Parse the string into a datetime object
+date_object = [datetime.strptime(datetimeElement, date_format)
+               for datetimeElement in datetimeList]
+
+time_interval = [(date_object[i] - date_object[i-1]).total_seconds()
+                 for i in range(1, len(date_object))]
+
+
+st.line_chart(time_interval)
+
+'Média do intervalo'
+mean_value = sum(time_interval) / len(time_interval)
+mean_value
+'Média do erro'
+mean_error = sum(abs(x - mean_value)
+                 for x in time_interval) / len(time_interval)
+mean_error/0.0001
