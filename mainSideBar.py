3b554e8daa2cb31f70168c466c3a8fb7d387d7e6
Nélio Dias <jrneliodias@gmail.com>
2023-09-09 23:09:44 -0300

    create MainSide Bar

---
+++ mainSideBar.py
@@ -0,0 +1,72 @@
+from connections import *
+import streamlit as st
+import pandas as pd
+import datetime
+
+
+def mainSidebarMenu():
+    st.write('#### Componentes Seriais Encontrados:')
+
+    portlist = get_ports()
+    for port_info in portlist:
+        st.write(
+            f"Description: {port_info.description}")
+
+    port_option = st.selectbox(
+        'Qual porta deseja conectar?',
+        [port_info.device for port_info in portlist])
+
+    col11, col12 = st.columns(2)
+    with col11:
+        if st.button('Conectar'):
+            serialPortValidationToConnect(port_option)
+
+    with col12:
+        if st.button('Desconectar'):
+            serialPortValidationToDisconnect()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        sampling_time_input = st.number_input(
+            'Tempo de amostragem:',
+            value=0.1,
+            min_value=0.005)
+        if sampling_time_input:
+            st.session_state.sampling_time = sampling_time_input
+
+    with col2:
+
+        samples_number = st.number_input(
+            'Quantidade de amostras:', value=100, step=10, min_value=1)
+
+        if samples_number:
+            st.session_state.samples_number = samples_number
+
+    if st.button('Receber Dados'):
+        if 'arduinoData' in st.session_state.connected:
+            arduinoData = st.session_state.connected['arduinoData']
+            st.session_state.sensor = dict()
+            sensor = st.session_state.sensor
+
+            start_time = time.time()
+            interation = 0
+            sampling_time = st.session_state.sampling_time
+            progress_text = "Operation in progress. Please wait."
+            my_bar = st.progress(0, text=progress_text)
+
+            while interation < samples_number:
+                current_time = time.time()
+                if current_time - start_time > sampling_time:
+                    start_time = current_time
+                    sendToArduino(arduinoData, 'ON')
+                    dataRead = readFromArduino(arduinoData)
+                    current_timestamp = datetime.datetime.now()
+                    sensor[str(current_timestamp)] = float(dataRead)
+                    interation += 1
+
+                    percent_complete = interation * 1 / samples_number
+
+                    my_bar.progress(percent_complete, text=progress_text)
+
+        else:
+            st.warning('Não há dispositivos conectados.')
