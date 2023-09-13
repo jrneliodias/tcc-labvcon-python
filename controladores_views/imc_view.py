3035ec0ddc5f5c707e712b7d60c93635c928a21f
Nélio Dias <jrneliodias@gmail.com>
2023-09-13 11:52:58 -0300

    imc controler second test - using the plant

--- controladores_views/imc_view.py
+++ controladores_views/imc_view.py
@@ -3,6 +3,8 @@ import pandas as pd
 from formatterInputs import *
 from control.matlab import tf, c2d, tfdata
 from numpy import exp, ones, zeros
+from connections import *
+import datetime
 
 
 def datetime_obj_to_elapsed_time():
@@ -26,18 +28,9 @@ def datetime_obj_to_elapsed_time():
 
 def imc_Controller():
 
-    sampling_time = st.session_state.sampling_time
-    samples_number = st.session_state.samples_number
-
     st.header('Controle IMC')
 
     col1, col2 = st.columns([0.7, 0.3])
-
-    with col1:
-
-        sensor_data_with_elapsed_time = datetime_obj_to_elapsed_time()
-
-        st.line_chart(data=sensor_data_with_elapsed_time, height=500)
 
     with col2:
 
@@ -60,105 +53,228 @@ def imc_Controller():
                 reference3 = st.number_input(
                     'Referência 3:', value=50, step=1, min_value=0, max_value=90, key='reference3_imc')
 
-        # IMC Controller Project
+        if st.button('Receber Dados'):
+            imcControlProcess()
 
-        # Initial Conditions
-        angulo_sensor = zeros(samples_number)
-        angulo_model1 = zeros(samples_number)
-        angulo_model2 = zeros(samples_number)
-        erro1 = zeros(samples_number)
-        erro2 = zeros(samples_number)
-        d0barra1 = zeros(samples_number)
-        d0barra2 = zeros(samples_number)
+    with col1:
 
-        # Ajuste do taumf1 e taumf2
-        ajuste1 = 0.6
-        ajuste2 = 0.6
+        sensor_data_with_elapsed_time = datetime_obj_to_elapsed_time()
 
-        angulo_ref = 50*ones(samples_number)
-        # Power Saturation
-        max_pot = 15
-        min_pot = 7
+        st.line_chart(data=sensor_data_with_elapsed_time, height=500)
 
-        # Variável controlada
-        pot_motor_1 = zeros(samples_number)
-        pot_motor_2 = zeros(samples_number)
-        delta_pot_motor_1 = zeros(samples_number)
-        delta_pot_motor_2 = zeros(samples_number)
-        u = ["0,0"]*samples_number
-        u
 
-        # Coefiientes do Modelo Smith motor 1
-        Kpsmith1 = 7.737
-        thetasmith1 = 0.65
-        tausmith1 = 0.6
+def imcControlProcess():
 
-        # Coefiientes do Modelo Smith motor 2
-        Kpsmith2 = 12.86
-        thetasmith2 = 0.5
-        tausmith2 = 0.66
+    sampling_time = st.session_state.sampling_time
+    samples_number = st.session_state.samples_number
+    # IMC Controller Project
 
-        # Motor 1 Model Transfer Function
-        Gm1 = tf(Kpsmith1, [tausmith1, 1])
-        Gmz1 = c2d(Gm1, sampling_time)
-        num1, den1 = tfdata(Gmz1)
-        Bm1 = num1[0][0]
-        Am1 = den1[0][0]
-        b0m1 = Bm1[0]
-        a1m1 = Am1[1]
+    # Initial Conditions
+    angulo_sensor = zeros(samples_number)
+    angulo_model1 = zeros(samples_number)
+    angulo_model2 = zeros(samples_number)
+    erro1 = zeros(samples_number)
+    erro2 = zeros(samples_number)
+    d0barra1 = zeros(samples_number)
+    d0barra2 = zeros(samples_number)
 
-        # Motor 2 Model Transfer Function
-        Gm2 = tf(Kpsmith2, [tausmith2, 1])
-        Gmz2 = c2d(Gm2, sampling_time)
-        num2, den2 = tfdata(Gmz2)
-        Bm2 = num2[0][0]
-        Am2 = den2[0][0]
-        b0m2 = Bm2[0]
-        a1m2 = Am2[1]
+    # taumf1 e taumf2 Ajusts
+    ajuste1 = 0.6
+    ajuste2 = 0.6
 
-        # Calculo do tau de malha fechada
-        tau_mf1 = ajuste1*tausmith1
-        alpha1 = exp(-sampling_time/tau_mf1)
+    # Setpoint
+    angulo_ref = 50*ones(samples_number)
 
-        tau_mf2 = ajuste2*tausmith2
-        alpha2 = exp(-sampling_time/tau_mf2)
+    # Power Saturation
+    max_pot = 15
+    min_pot = 7
 
-        for k in range(2, samples_number):
+    # Variável controlada
+    pot_motor_1 = zeros(samples_number)
+    pot_motor_2 = zeros(samples_number)
+    motors_power_packet = ["0,0"]*samples_number
 
-            # ----- Sa�da da planta
-            angulo_sensor[k] = -a1m1*angulo_model1[k-1] + b0m1*pot_motor_1[k-1]
+    # Coefiientes do Modelo Smith motor 1
+    Kpsmith1 = 7.737
+    thetasmith1 = 0.65
+    tausmith1 = 0.6
 
-            # ---- Saida do modelo
-            angulo_model1[k] = -a1m1*angulo_model1[k-1] + b0m1*pot_motor_1[k-1]
-            angulo_model2[k] = -a1m2*angulo_model2[k-1] - b0m2*pot_motor_2[k-1]
+    # Coefiientes do Modelo Smith motor 2
+    Kpsmith2 = 12.86
+    thetasmith2 = 0.5
+    tausmith2 = 0.66
 
-            # Determinar a incerteza
-            d0barra1[k] = angulo_sensor[k] - angulo_model1[k]
-            d0barra2[k] = -(angulo_sensor[k] - angulo_model2[k])
+    # Motor 1 Model Transfer Function
+    Gm1 = tf(Kpsmith1, [tausmith1, 1])
+    Gmz1 = c2d(Gm1, sampling_time)
+    num1, den1 = tfdata(Gmz1)
+    Bm1 = num1[0][0]
+    Am1 = den1[0][0]
+    b0m1 = Bm1[0]
+    a1m1 = Am1[1]
 
-            # Determinar o Erro
-            erro1[k] = angulo_ref[k] - d0barra1[k]
-            erro2[k] = -(angulo_ref[k] + d0barra2[k])
+    # Motor 2 Model Transfer Function
+    Gm2 = tf(Kpsmith2, [tausmith2, 1])
+    Gmz2 = c2d(Gm2, sampling_time)
+    num2, den2 = tfdata(Gmz2)
+    Bm2 = num2[0][0]
+    Am2 = den2[0][0]
+    b0m2 = Bm2[0]
+    a1m2 = Am2[1]
 
-            # Sinal de controle
-            pot_motor_1[k] = alpha1*pot_motor_1[k-1] + ((1-alpha1)/b0m1)*erro1[k-1] + \
-                a1m1*((1-alpha1)/b0m1)*erro1[k-2]
+    # Close Loop Tau Calculation
+    tau_mf1 = ajuste1*tausmith1
+    alpha1 = exp(-sampling_time/tau_mf1)
 
-            pot_motor_2[k] = alpha2*pot_motor_2[k-1] + ((1-alpha2)/b0m2)*erro2[k-1] + \
-                a1m2*((1-alpha2)/b0m2)*erro2[k-2]
+    tau_mf2 = ajuste2*tausmith2
+    alpha2 = exp(-sampling_time/tau_mf2)
 
-            # Satura��es do sinal de Controle
-            pot_motor_1[k] = max(min_pot, min(pot_motor_1[k], max_pot))
-            pot_motor_2[k] = max(min_pot, min(pot_motor_2[k], max_pot))
+    # Receber o objeto arduino da sessão
+    arduinoData = st.session_state.connected['arduinoData']
 
-            # Enviar sinal de controle para os motores
-            u[k] = f"{pot_motor_1[k]},{pot_motor_2[k]}\r"
-            u
+    # limpar os valores anteriores do sensor
+    st.session_state.sensor = dict()
+    sensor = st.session_state.sensor
 
-            # Retirar dados do sensor errado
-            if angulo_sensor[k] <= 0 or angulo_sensor[k] > 90:
-                # Tratar os dados errados
-                angulo_sensor[k] = angulo_sensor[k-1]
+    # inicializar  o timer
+    start_time = time.time()
+    interation = 0
 
-    st.line_chart(angulo_sensor)
-    st.line_chart(pot_motor_1)
+    # Receber os valores de tempo de amostragem e número de amostras da sessão
+    sampling_time = st.session_state.sampling_time
+    samples_number = st.session_state.samples_number
+
+    # Inicializar a barra de progresso
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+    sendToArduino(arduinoData, '0,0')
+
+    while interation < samples_number:
+        current_time = time.time()
+        if current_time - start_time > sampling_time:
+            start_time = current_time
+            # -----  Angle Sensor Output
+            angulo_sensor[interation] = readFromArduino(arduinoData)
+
+            # ---- Motor Model Output
+            angulo_model1[interation] = -a1m1 * \
+                angulo_model1[interation-1] + \
+                b0m1*pot_motor_1[interation-1]
+            angulo_model2[interation] = -a1m2 * \
+                angulo_model2[interation-1] - \
+                b0m2*pot_motor_2[interation-1]
+
+            # Determine uncertainty
+            d0barra1[interation] = angulo_sensor[interation] - \
+                angulo_model1[interation]
+            d0barra2[interation] = - \
+                (angulo_sensor[interation] - angulo_model2[interation])
+
+            # Determine Error
+            erro1[interation] = angulo_ref[interation] - \
+                d0barra1[interation]
+            erro2[interation] = - \
+                (angulo_ref[interation] + d0barra2[interation])
+
+            # Control Signal
+            pot_motor_1[interation] = alpha1*pot_motor_1[interation-1] + ((1-alpha1)/b0m1)*erro1[interation-1] + \
+                a1m1*((1-alpha1)/b0m1)*erro1[interation-2]
+
+            pot_motor_2[interation] = alpha2*pot_motor_2[interation-1] + ((1-alpha2)/b0m2)*erro2[interation-1] + \
+                a1m2*((1-alpha2)/b0m2)*erro2[interation-2]
+
+            # Control Signal Saturation
+            pot_motor_1[interation] = max(
+                min_pot, min(pot_motor_1[interation], max_pot))
+            pot_motor_2[interation] = max(
+                min_pot, min(pot_motor_2[interation], max_pot))
+
+            # Motor Power String Formatation
+            motors_power_packet[interation] = f"{pot_motor_1[interation]},{pot_motor_2[interation]}\r"
+
+            # Clean Sensor Data
+            if angulo_sensor[interation] <= 0 or angulo_sensor[interation] > 90:
+                angulo_sensor[interation] = angulo_sensor[interation-1]
+
+            sendToArduino(arduinoData, motors_power_packet[interation])
+            current_timestamp = datetime.datetime.now()
+            sensor[str(current_timestamp)] = float(angulo_sensor[interation])
+            interation += 1
+
+            percent_complete = interation * 1 / samples_number
+            my_bar.progress(percent_complete, text=progress_text)
+
+    sendToArduino(arduinoData, '0,0')
+
+    # for k in range(1, samples_number):
+
+    #     # -----  Angle Sensor Output
+    #     angulo_sensor[k] = -a1m1*angulo_model1[k-1] + b0m1*pot_motor_1[k-1]
+
+    #     # ---- Motor Model Output
+    #     angulo_model1[k] = -a1m1*angulo_model1[k-1] + b0m1*pot_motor_1[k-1]
+    #     angulo_model2[k] = -a1m2*angulo_model2[k-1] - b0m2*pot_motor_2[k-1]
+
+    #     # Determine uncertainty
+    #     d0barra1[k] = angulo_sensor[k] - angulo_model1[k]
+    #     d0barra2[k] = -(angulo_sensor[k] - angulo_model2[k])
+
+    #     # Determine Error
+    #     erro1[k] = angulo_ref[k] - d0barra1[k]
+    #     erro2[k] = -(angulo_ref[k] + d0barra2[k])
+
+    #     # Control Signal
+    #     pot_motor_1[k] = alpha1*pot_motor_1[k-1] + ((1-alpha1)/b0m1)*erro1[k-1] + \
+    #         a1m1*((1-alpha1)/b0m1)*erro1[k-2]
+
+    #     pot_motor_2[k] = alpha2*pot_motor_2[k-1] + ((1-alpha2)/b0m2)*erro2[k-1] + \
+    #         a1m2*((1-alpha2)/b0m2)*erro2[k-2]
+
+    #     # Control Signal Saturation
+    #     pot_motor_1[k] = max(min_pot, min(pot_motor_1[k], max_pot))
+    #     pot_motor_2[k] = max(min_pot, min(pot_motor_2[k], max_pot))
+
+    #     # Motor Power String Formatation
+    #     motors_power_packet[k] = f"{pot_motor_1[k]},{pot_motor_2[k]}\r"
+
+    #     # Clean Sensor Data
+    #     if angulo_sensor[k] <= 0 or angulo_sensor[k] > 90:
+    #         angulo_sensor[k] = angulo_sensor[k-1]
+
+
+def enviarDados(controlSignal: str = '0,0'):
+    if 'sensor' not in st.session_state:
+        return st.error('Não há dispositivos conectados.')
+
+    # Receber o objeto arduino da sessão
+    arduinoData = st.session_state.connected['arduinoData']
+
+    # limpar os valores anteriores do sensor
+    st.session_state.sensor = dict()
+    sensor = st.session_state.sensor
+
+    # inicializar  o timer
+    start_time = time.time()
+    interation = 0
+
+    # Receber os valores de tempo de amostragem e número de amostras da sessão
+    sampling_time = st.session_state.sampling_time
+    samples_number = st.session_state.samples_number
+
+    # Inicializar a barra de progresso
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+
+    while interation < samples_number:
+        current_time = time.time()
+        if current_time - start_time > sampling_time:
+            start_time = current_time
+
+            sendToArduino(arduinoData, controlSignal)
+            dataRead = readFromArduino(arduinoData)
+            current_timestamp = datetime.datetime.now()
+            sensor[str(current_timestamp)] = float(dataRead)
+            interation += 1
+
+            percent_complete = interation * 1 / samples_number
+            my_bar.progress(percent_complete, text=progress_text)
