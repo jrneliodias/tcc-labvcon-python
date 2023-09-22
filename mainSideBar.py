79b047af60eb11128ecdd4a94a2c3f470eed5796
Nélio Dias <jrneliodias@gmail.com>
2023-09-22 11:47:45 -0300

    create saturation inputs on sidebar

--- mainSideBar.py
+++ mainSideBar.py
@@ -1,7 +1,5 @@
 from connections import *
 import streamlit as st
-import pandas as pd
-import datetime
 
 
 def mainSidebarMenu():
@@ -26,19 +24,36 @@ def mainSidebarMenu():
         if st.button('Desconectar'):
             serialPortValidationToDisconnect()
     st.divider()
+    
     col1, col2 = st.columns(2)
+    
+    
     with col1:
         sampling_time_input = st.number_input(
-            'Tempo de amostragem:',
+            'Tempo de amostragem (s):',
             value=0.1,
             min_value=0.005)
+        
+      
+        
+        saturation_min_value = st.number_input('Saturação mínima:',value=0,
+            min_value=0)
+        
+        st.session_state.controller_parameters['saturation_min_value'] = saturation_min_value
         if sampling_time_input:
-            st.session_state.sampling_time = sampling_time_input
+            st.session_state.controller_parameters['sampling_time'] = sampling_time_input
 
     with col2:
 
         samples_number = st.number_input(
             'Quantidade de amostras:', value=100, step=10, min_value=1)
-
+        
+        saturation_max_value = st.number_input('Saturação Máxima:',value=15,
+            min_value=0,max_value=256)
+        
+        st.session_state.controller_parameters['saturation_max_value'] = saturation_max_value
+        
         if samples_number:
-            st.session_state.samples_number = samples_number
+            st.session_state.controller_parameters['samples_number'] = samples_number
+            
+    st.write(f'## Tempo total de Simulação:   {sampling_time_input*samples_number:.2f} s')
