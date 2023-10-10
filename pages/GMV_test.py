c163a0ca030e37c3e8b40bb6ceae1dd8f5cef290
Nélio Dias <jrneliodias@gmail.com>
2023-10-10 09:50:08 -0300

    GMV test in streamlit

--- pages/GMV_test.py
+++ pages/GMV_test.py
@@ -11,7 +11,7 @@ arduinoData = st.session_state.connected['arduinoData']
 
 
 sampling_time = 0.01
-samples_number = 2000
+samples_number = 5000
 
 # Initial Conditions
 output_sensor = np.zeros(samples_number)
@@ -20,7 +20,8 @@ delta_control_signal = np.zeros(samples_number)
 
 # Setpoint
 reference = 50*np.ones(samples_number)
-#reference[1000:] = 70
+reference[4000:] = 70
+q01 = 300
 
 # Power Saturation
 max_pot = 200
@@ -33,22 +34,34 @@ motors_power_packet = ["0"]*samples_number
 # Coefiientes do Modelo Smith motor 1
 Kpsmith1 = 0.999 #7.737
 thetasmith1 = 0.65
-tausmith1 = 1.334 #0.6
+tausmith1 = 1.827 #0.6
 
-# Coefiientes do Modelo Smith motor 2
-Kpsmith2 = 12.86
-thetasmith2 = 0.5
-tausmith2 = 0.66
 
 # Motor 1 Model Transfer Function
-Gm1 = tf(Kpsmith1, [tausmith1, 1])
+#Gm1 = tf(Kpsmith1, [tausmith1, 1])
+Gm1 = tf(1, [1, 2*0.9, 1]) # Second Order Transfer Function
 Gmz1 = c2d(Gm1, sampling_time)
 num1, den1 = tfdata(Gmz1)
 Bm1 = num1[0][0]
+
+"Bm1"
+Bm1
 Am1 = den1[0][0]
+"Am1"
+Am1
+a1m1 = Am1[1]
+st.write(f'a1m1 = {a1m1}')
+
+b0m1 = Bm1[0]
+st.write(f'b0m1 = {b0m1}')
 
 A_order = len(Am1)-1
-B_order = len(Bm1) # Zero holder aumenta um grau
+B_order = len(Bm1)-1 
+
+# Coefiientes do Modelo Smith motor 2
+# Kpsmith2 = 12.86
+# thetasmith2 = 0.5
+# tausmith2 = 0.66
 
 # Motor 2 Model Transfer Function
 # Gm2 = tf(Kpsmith2, [tausmith2, 1])
@@ -76,7 +89,7 @@ B_order = len(Bm1) # Zero holder aumenta um grau
 def create_P1_poly(ne,ns):
     P1 = np.zeros((ne + ns + 2,1))
     P1[0] = 1
-    P1[1] = -0.98
+    #P1[1] = -0.98
     return P1
 
 
@@ -105,7 +118,8 @@ def E_S_poly_calculation(ne,ns,Am1,P):
 # Calculo do polinômio R1(z^-1)
 def r_poly_calculation(Bm1,epoly1,q0):
     BE_poly = np.convolve(Bm1,epoly1)
-    rpoly1 = BE_poly + q0
+    BE_poly[0] += q0
+    rpoly1 = BE_poly
     return rpoly1
 
 d = 1
@@ -114,28 +128,45 @@ nb1 = B_order
 ns1 = na1
 ne1 = d-1
 
-q01 = 1
 
 # Definir o polinômio P1(z)
 P1 = create_P1_poly(ne1,ns1)
-
 # Encontrar o polinômio E1(z) e S1(z)
 e_poly_1,s_poly_1 = E_S_poly_calculation(ne1,ns1,Am1,P1)
+"e_poly_1"
+e_poly_1
+"s_poly_1"
+s_poly_1
+"convolve(Bm1,e_poly_1)"
+st.write(np.convolve(Bm1,e_poly_1))
 
 # Encontrar o polinômio R por meio do Q1(z^-1) e B1(z^-1)
 r_poly_1 = r_poly_calculation(Bm1,e_poly_1,q01)
+"r_poly_1"
+r_poly_1
 nr1 = len(r_poly_1)-1
 
 # Calculo do polinômio T1(z)
-t01 = sum(P1)[0];
+t01 = sum(P1)[0]
+
+# Determinar E e S
+e0 = 1
+s0 = -(a1m1-1)*e0
+st.write(f's0 = {s0}')
+s1 = a1m1*e0
+st.write(f's1 = {s1}')
+r0 = b0m1+q01
+st.write(f'r0 = {r0}')
 
 
 # inicializar  o timer
 start_time = time.time()
-kk = 2
-
+kk = 3
 
 if st.button('Iniciar Simulação'):
+    # Inicializar a barra de progresso
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
     
     sendToArduino(arduinoData, '0')
 
@@ -147,8 +178,11 @@ if st.button('Iniciar Simulação'):
             output_sensor[kk] = readFromArduino(arduinoData)
 
             # GMV Control Signal
-            delta_control_signal[kk] = (t01*reference[kk] - np.dot(s_poly_1,output_sensor[kk:kk-ns1-1:-1]))/r_poly_1[0]
-
+            delta_control_signal[kk] = np.dot(r_poly_1[1:],delta_control_signal[kk-1:kk-nb1-1:-1]) + t01*reference[kk] - np.dot(s_poly_1,output_sensor[kk:kk-ns1-1:-1])
+            delta_control_signal[kk]/=r_poly_1[0]
+            
+            #delta_control_signal[kk] = (t01*reference[kk] - (s0*output_sensor[kk] + s1*output_sensor[kk-1]))/r0
+            #  deltaU(k) = (-r1/r0)*deltaU(k-1) + (t0/r0)*ref(k) - (s0/r0)*y(k) -(s1/r0)*y(k-1)-(s2/r0)*y(k-2);
             control_signal[kk] = control_signal[kk-1] +  delta_control_signal[kk]
             
             # Control Signal Saturation
@@ -164,6 +198,8 @@ if st.button('Iniciar Simulação'):
             sendToArduino(arduinoData, motors_power_packet[kk])
             
             kk += 1
+            percent_complete = kk / (samples_number)
+            my_bar.progress(percent_complete, text=progress_text)
 
 
     sendToArduino(arduinoData, '0')
