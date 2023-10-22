d2a2f65dc39fdae580392d919409eb911262a10b
Nélio Dias <jrneliodias@gmail.com>
2023-10-22 16:50:14 -0300

    refactoring convert_tf_2_discrete function to FGPC

--- controllers_process/validations_functions.py
+++ controllers_process/validations_functions.py
@@ -4,7 +4,8 @@ from control.matlab import tf, c2d, tfdata
 from connections import *
 from session_state import get_session_variable
 from controllers_process.performace_metrics import *
-    
+from numpy import convolve,array
+
 def coefficients_validations(coeff_string):
     if  coeff_string == '':
         return None
@@ -20,25 +21,28 @@ def plot_chart_validation(plot_variable,y:str,height= 200, x = 'Time (s)'):
     
     return st.line_chart(data= plot_variable,x = x, y = y,height=height)
 
-def convert_tf_2_discrete(num_coeff:str,den_coeff:str,tf_type:str):
+def convert_tf_2_discrete(num_coeff:str,den_coeff:str,tf_type:str,f_gpc_mimo_checkbox:bool, K_alpha:float|None  = 0, alpha_fgpc:float|None = 0):
+    
+    sampling_time = get_session_variable('sampling_time')
     num_coeff_float = string2floatArray(num_coeff)
     den_coeff_float = string2floatArray(den_coeff)
-    sampling_time = get_session_variable('sampling_time')
+    
 
     if tf_type == 'Discreto':
-        Gm1 = tf(num_coeff_float, den_coeff_float,sampling_time)
-        num1, den1 = tfdata(Gm1)
-        Bm1 = num1[0][0]
-        Am1 = den1[0][0]
-        return Am1, Bm1
-    
+        Gm1 = tf(num_coeff_float, den_coeff_float,sampling_time)   
     
     # Motor 1 Model Transfer Function
     Gm1 = tf(num_coeff_float, den_coeff_float)
     Gmz1 = c2d(Gm1, sampling_time)
     num1, den1 = tfdata(Gmz1)
-    Bm1 = num1[0][0]
     Am1 = den1[0][0]
+    Bm1 = num1[0][0]
+    
+    if f_gpc_mimo_checkbox:
+        alpha_filter = K_alpha*array([1,-alpha_fgpc])
+        Bm1 = convolve(Bm1,alpha_filter)
+
+    
     return Am1, Bm1
 
 def validateFloatInput(input_numbers_str:str):
